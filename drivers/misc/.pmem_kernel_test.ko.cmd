4da8cd79892a15b4e6a1a9bc67cfeafab9b7667c
RoQ <chroque@gmail.com>
2012-03-28 23:31:31 -0500

    Initial commit - kernel 2.6.32 Optimus ME

---
+++ drivers/misc/.pmem_kernel_test.ko.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/misc/pmem_kernel_test.ko := /home/roq/android-prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-ld -EL -r  -T /home/roq/pecan/scripts/module-common.lds --build-id -o drivers/misc/pmem_kernel_test.ko drivers/misc/pmem_kernel_test.o drivers/misc/pmem_kernel_test.mod.o
---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Wed Mar 28 23:16:37 PET 2012"
+#define LINUX_COMPILE_TIME "23:16:37"
+#define LINUX_COMPILE_BY "lg-electronics"
+#define LINUX_COMPILE_HOST "roq"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
