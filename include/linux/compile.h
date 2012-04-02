c023fe74467b5609133909064793a80d448f1180
RoQ <chroque@gmail.com>
2012-04-01 22:57:38 -0500

    update files

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 13 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#10 PREEMPT Thu Mar 29 22:17:53 PET 2012"
-#define LINUX_COMPILE_TIME "22:17:53"
-#define LINUX_COMPILE_BY "lg-electronics"
+#define UTS_VERSION "#13 PREEMPT Sun Apr 1 22:54:11 PET 2012"
+#define LINUX_COMPILE_TIME "22:54:11"
+#define LINUX_COMPILE_BY "mioptimusone.com"
 #define LINUX_COMPILE_HOST "roq"
 #define LINUX_COMPILE_DOMAIN
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/linux/utsrelease.h
+++ include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.32.9v1"
+#define UTS_RELEASE "2.6.32.9-v2"
